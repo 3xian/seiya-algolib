c1dea9c5e6641021790701c8f6e8c763cb3ae609
suffixtree@gmail.com <suffixtree@gmail.com@c6324bbd-cf03-bca1-b668-174533942762>
2011-09-25 02:39:55 +0000

    ..

--- trunk/util/default.cc
+++ trunk/util/default.cc
@@ -12,21 +12,21 @@
 #include <sstream>
 #include <stack>
 #include <vector>
-#define PREV(i, n) ((i)>0?(i)-1:(n)-1)
-#define NEXT(i, n) ((i)+1<(n)?(i)+1:0)
-#ifdef  LOCAL
+#ifndef ONLINE_JUDGE
 #define debug(x) cout<<"  ... "#x" : "<<(x)<<"\n"
 #else
 #define debug(x) 
 #endif
+#define SQ(x) ((x)*(x))
+#define PV(i,n) ((i)>0?(i)-1:(n)-1)
+#define NX(i,n) ((i)+1<(n)?(i)+1:0)
 using namespace std;
 typedef int64_t ll;
 typedef pair<int, int> pii;
-template <typename T> T sqr(const T& x) { return x * x; }
-template <typename T> void mil(T& a, const T& b) { if (a > b) a = b; }
-template <typename T> void mal(T& a, const T& b) { if (a < b) a = b; }
-template <typename T> void usort(vector<T>& a) { sort(a.begin(), a.end()); a.erase(unique(a.begin(), a.end()), a.end()); }
-template <typename A, typename B> B conv(const A& i) { stringstream s; s << i; B o; s >> o; return o; }
+template<typename T> void mil(T& a, const T& b) { if (a > b) a = b; }
+template<typename T> void mal(T& a, const T& b) { if (a < b) a = b; }
+template<typename T> void usort(vector<T>& a) { sort(a.begin(), a.end()); a.erase(unique(a.begin(), a.end()), a.end()); }
+template<typename A, typename B> B conv(const A& i) { stringstream s; s << i; B o; s >> o; return o; }
 
 int main() {
   return 0;
