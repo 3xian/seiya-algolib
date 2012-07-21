72a281b6378e83700bfbf93561ba521b772398de
suffixtree@gmail.com <suffixtree@gmail.com@c6324bbd-cf03-bca1-b668-174533942762>
2012-07-21 05:03:35 +0000

    :D

--- collections/sbtree.h
+++ collections/sbtree.h
@@ -1,142 +1,83 @@
 template<typename T>
 struct SBNode {
   int size;
+  SBNode<T>* child[2];
   T key;
-  SBNode<T>* lchild;
-  SBNode<T>* rchild;
   SBNode() { }
-  SBNode(SBNode<T>* l, SBNode<T>* r, int s, const T& k)
-      : lchild(l), rchild(r), size(s), key(k) { }
+  SBNode(SBNode<T>* l, SBNode<T>* r, int s, const T& k) : size(s), key(k) {
+    child[0] = l;
+    child[1] = r;
+  }
 };
 
-template<typename Type>
+template<typename T>
 class SBTree {
  public:
   SBTree();
   ~SBTree();
-
-  void insert( Type key );
-  Type get_rank( int k  ); // [0,size)
-  void clear();
-  void travel();
+  int size();
+  void insert(const T& key);
 
  private:
-  void left_rotate ( SBNode<Type>*& root );
-  void right_rotate( SBNode<Type>*& root );
-  void maintain( SBNode<Type>*& root, bool style );
-  void insert( SBNode<Type>*& root, Type key );
-  void clear ( SBNode<Type>* root );
-  void travel( SBNode<Type>* root );
+  void maintain(SBNode<T>*& cur, int style) const;
+  void rotate(SBNode<T>*& cur, int direct) const;
+  void insert(SBNode<T>*& cur, const T& key);
 
-  SBNode<Type>* root;
-  SBNode<Type>* _null;
+  SBNode<T>* _root;
+  SBNode<T>* _null;
 };
 
-template<typename Type>
-Type SBTree<Type>::get_rank( int k ){
-  SBNode<Type>* tmp= root;
-  for( ;; ){
-    if( tmp->lchild->size> k ) tmp= tmp->lchild;
-    else if( k> tmp->lchild->size ){
-      k-= tmp->lchild->size+ 1;
-      tmp= tmp->rchild;
-    } else break;
-  }
-  return tmp->key;
+template<typename T>
+SBTree<T>::SBTree() {
+  _null= new SBNode<T>();
+  _root= _null; 
+  _root->child[0] = _null;
+  _root->child[1] = _null;
+  _root->size= 0;
 }
 
-template<typename Type>
-void SBTree<Type>::clear( SBNode<Type>* root ){
-  if( root!= _null ){
-    clear( root->lchild );
-    clear( root->rchild );
-    delete root; }
+template<typename T>
+SBTree<T>::~SBTree() { }
+
+template<typename T>
+inline int SBTree<T>::size() {
+  return _root->size;
 }
 
-template<typename Type>
-void SBTree<Type>::clear(){
-  clear(root);
-  delete _null;
+template<typename T>
+void SBTree<T>::insert(const T& key) {
+  insert(_root, key);
 }
 
-template<typename Type>
-void SBTree<Type>::travel( SBNode<Type>* root ){
-  if ( root!= _null ){
-    travel( root->lchild );
-    cout << root->key << "  ";
-    travel( root->rchild );
-  }
+template<typename T>
+inline void SBTree<T>::insert(SBNode<T>*& cur, const T& key){
+  if (cur == _null) { cur = new SBNode<T>( _null, _null, 1, key); return; }
+  ++cur->size;
+  insert(cur->child[key >= cur->key], key);
+  maintain(cur, key >= cur->key);
 }
 
-template<typename Type>
-void SBTree<Type>::travel(){
-  travel( root );
+template<typename T>
+inline void SBTree<T>::rotate(SBNode<T>*& cur, int dir) const {
+  SBNode<T>* tmp= cur->child[dir^1];
+  cur->child[dir^1]= tmp->child[dir];
+  tmp->child[dir]= cur;
+  tmp->size = cur->size;
+  cur->size = cur->child[0]->size+ cur->child[1]->size+ 1;
+  cur = tmp;
 }
 
-template<typename Type>
-void SBTree<Type>::insert( SBNode<Type>*& root, Type key ){
-  if( root== _null ){
-    root= new SBNode<Type>( _null, _null, 1, key ); return; }
-  root->size++;
-  if( key< root->key ) insert( root->lchild, key );
-  else                 insert( root->rchild, key );
-  maintain( root, key>= root->key );
-}
-
-template<typename Type>
-void SBTree<Type>::insert( Type key ){
-  insert( root, key );
-}
-
-template<typename Type>
-SBTree<Type>::SBTree(){
-  _null= new SBNode<Type>();
-  root= _null; 
-  root->lchild= root->rchild= _null;
-  root->size= 0;
-}
-
-template<typename Type>
-SBTree<Type>::~SBTree(){
-  clear();
-}
-
-template<typename Type>
-inline void SBTree<Type>::left_rotate( SBNode<Type>*& root ){
-  SBNode<Type>* tmp= root->rchild;
-  root->rchild= tmp->lchild;  tmp->lchild= root;
-  tmp->size= root->size;
-  root->size= root->lchild->size+ root->rchild->size+ 1;
-  root= tmp;
-}
-
-template<typename Type>
-inline void SBTree<Type>::right_rotate( SBNode<Type>*& root ){
-  SBNode<Type>* tmp= root->lchild;
-  root->lchild= tmp->rchild;  tmp->rchild= root;
-  tmp->size= root->size;
-  root->size= root->lchild->size+ root->rchild->size+ 1;
-  root= tmp;
-}
-
-template<typename Type>
-void SBTree<Type>::maintain( SBNode<Type>*& root, bool style ){
-  if( root== _null ) return;
-  if( !style ){
-    if( root->lchild->lchild->size> root->rchild->size )
-      right_rotate( root );
-    else if( root->lchild->rchild->size> root->rchild->size ){
-      left_rotate( root->lchild );
-      right_rotate( root );
-    }else return;
-  } else {
-    if( root->rchild->rchild->size> root->lchild->size )
-      left_rotate( root );
-    else if( root->rchild->lchild->size> root->lchild->size ){
-      right_rotate( root->rchild );
-      left_rotate( root ); 
-    }else return;
-  }
-  maintain(root->lchild, false);  maintain(root->rchild, true);
-  maintain(root, false);          maintain(root, true);
+template<typename T>
+inline void SBTree<T>::maintain(SBNode<T>*& cur, int style) const {
+  if (cur == _null) return;
+  if (cur->child[style]->child[style]->size> cur->child[style^1]->size)
+    rotate(cur, style^1);
+  else if (cur->child[style]->child[style^1]->size> cur->child[style^1]->size){
+    rotate(cur->child[style], style);
+    rotate(cur, style^1);
+  } else return;
+  maintain(cur->child[0], 0);
+  maintain(cur->child[1], 1);
+  maintain(cur, 0);
+  maintain(cur, 1);
 }
