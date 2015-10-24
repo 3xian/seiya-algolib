a83d1903f4f35fd03151cadccb38056f3caa97fd
daiwei <david@cloudist.cc>
2015-10-24 23:12:45 +0800

    优化扩展欧几里得 Python 实现

--- math/euclid.py
+++ math/euclid.py
@@ -1,6 +1,5 @@
-def euclid(a, b, x):
-	if b == 0:
-		x[0], x[1] = 1, 0
-		return a 
-	d = euclid(b, a % b, x)
-	x[0], x[1] = x[1], x[0] - a / b * x[1]
+def euclid(a, b):
+    if b == 0:
+        return a, 1, 0
+    d, y, x = euclid(b, a % b)
+    return d, x, y-a/b*x
